2c49dda43600858be1e95b518acdc550ab552290
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-01-09 20:26:48 -0300

    rendering stitches after tailwind

--- stage06/src/pages/_app.page.tsx
+++ stage06/src/pages/_app.page.tsx
@@ -1,6 +1,6 @@
-import '../styles/global.css'
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
+import '../styles/global.css'
 
 globalStyles()
 
--- stage06/src/pages/_document.page.tsx
+++ stage06/src/pages/_document.page.tsx
@@ -1,4 +1,3 @@
-import { getCssText } from '@ignite-ui/react'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -14,10 +13,6 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
-        />
-        <style
-          id="stitches"
-          dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
       </Head>
       <body>
