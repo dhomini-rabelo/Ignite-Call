27edec50200cc2882f4645562bec0acc2ca4abb1
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-01-08 22:03:37 -0300

    add global styles using storybook

---
+++ stage06/src/styles/global.ts
@@ -0,0 +1,29 @@
+import { globalCss } from "@ignite-ui/react";
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+    fontFamily: 'Roboto',
+  },
+
+  'html, body': {
+    minWidth: '100%',
+    minHeight: '100%',
+    backgroundColor: '$gray-900',
+    color: '$gray-100',
+    '-webkit-font-smoothing': 'antialised',
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: 'Roboto',
+    fontWeight: 400
+  },
+
+  '.etc': {
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  }
+})
