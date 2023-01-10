54fa5ddfce2e2596ed144d80743789baa5f744d3
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-01-09 21:59:18 -0300

    add register page

--- stage06/src/code/schemas/validations/user.ts
+++ stage06/src/code/schemas/validations/user.ts
@@ -1,11 +1,21 @@
 import * as zod from 'zod'
+import { DynamicErrorMessage, ErrorMessages } from '../errors'
 
 export const claimUsernameSchema = zod.object({
   username: zod
     .string()
-    .min(3, 'Use mais que 3 letras')
+    .min(3, DynamicErrorMessage.minLength(3))
     .regex(/^([a-z\\-]+)/i, 'Use somente letras e hífen')
     .transform((v) => v.toLocaleLowerCase()),
 })
 
 export type IClaimUsernameSchema = zod.infer<typeof claimUsernameSchema>
+
+export const registerSchema = claimUsernameSchema.extend({
+  name: zod
+    .string()
+    .min(1, ErrorMessages.REQUIRED)
+    .transform((v) => v.toLocaleLowerCase()),
+})
+
+export type IRegisterSchema = zod.infer<typeof registerSchema>
---
+++ stage06/src/pages/register/index.page.tsx
@@ -0,0 +1,72 @@
+import {
+  Box,
+  Button,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  IRegisterSchema,
+  registerSchema,
+} from '../../code/schemas/validations/user'
+import { Form } from '../../styles/form'
+
+export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IRegisterSchema>({
+    resolver: zodResolver(registerSchema),
+  })
+
+  function onValidSubmit(data: IRegisterSchema) {
+    console.log(data)
+  }
+  return (
+    <main className="max-w-[572px] mt-20 mx-auto mb-4 px-4">
+      <header>
+        <Heading as="h1" className="font-bold">
+          Bem-vindo ao Ignite Call!
+        </Heading>
+        <Text className="text-Gray-200 mb-6">
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={1} />
+      </header>
+      <Box
+        as="form"
+        className="mt-6 flex flex-col gap-4 px-0"
+        onSubmit={handleSubmit(onValidSubmit)}
+      >
+        <label className="flex flex-col gap-2">
+          <Text size="sm">Nome do usuário</Text>
+          <TextInput
+            prefix="ignite.com/"
+            placeholder="Seu usuário"
+            {...register('username')}
+          />
+          <Form.error>
+            {errors.username ? errors.username.message : <>&nbsp;</>}
+          </Form.error>
+        </label>
+        <label className="flex flex-col gap-2">
+          <Text size="sm">Nome completo</Text>
+          <TextInput placeholder="Seu nome" {...register('name')} />
+          <Form.error>
+            {errors.name ? errors.name.message : <>&nbsp;</>}
+          </Form.error>
+        </label>
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </Box>
+    </main>
+  )
+}
