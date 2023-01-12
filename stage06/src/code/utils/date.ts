347c5760567b8ecb27864ecad61c4dee321452ab
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-01-12 20:47:00 -0300

    create utils/date

---
+++ stage06/src/code/utils/date.ts
@@ -0,0 +1,9 @@
+export function getWeekDays() {
+  const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
+
+  return Array.from(Array(7).keys())
+    .map((dayNumber) =>
+      formatter.format(new Date(Date.UTC(2021, 5, dayNumber))),
+    )
+    .map((dayName) => dayName[0].toUpperCase() + dayName.slice(1))
+}
