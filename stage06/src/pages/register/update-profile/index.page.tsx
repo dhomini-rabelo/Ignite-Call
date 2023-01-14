b206c126a5656eab85afcb00ac575397437bda0b
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-01-13 21:46:06 -0300

    add handleUpdateUser in /register/update-profile

--- stage06/src/pages/register/update-profile/index.page.tsx
+++ stage06/src/pages/register/update-profile/index.page.tsx
@@ -11,28 +11,33 @@ import {
 import { GetServerSideProps } from 'next'
 import { unstable_getServerSession } from 'next-auth'
 import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import {
   IUpdateUserSchema,
   updateUserSchema,
 } from '../../../code/schemas/validations/user'
+import { client } from '../../../code/settings/frontend'
 import { buildNextAuthOptions } from '../../api/auth/[...nextauth].api'
 
 export default function TimerIntervals() {
   const {
     register,
-    reset,
     handleSubmit,
-    setValue,
-    setError,
-    formState: { errors, isSubmitting },
+    formState: { isSubmitting },
   } = useForm<IUpdateUserSchema>({
     resolver: zodResolver(updateUserSchema),
   })
-
   const session = useSession()
-  console.log(session)
+  const router = useRouter()
+
+  async function handleUpdateUser(data: IUpdateUserSchema) {
+    await client.put('users/update-profile', {
+      bio: data.bio,
+    })
+    await router.push(`/schedule/${session.data?.user.username}`)
+  }
 
   return (
     <main className="max-w-[572px] mt-20 mx-auto mb-4 px-4">
@@ -45,7 +50,18 @@ export default function TimerIntervals() {
         </Text>
         <MultiStep size={4} currentStep={4} />
       </header>
-      <Box as="form" className="flex flex-col mt-6 px-0 gap-4">
+      <Box
+        as="form"
+        className="flex flex-col mt-6 px-0 gap-4"
+        onSubmit={handleSubmit(handleUpdateUser)}
+      >
+        <label className="flex flex-col gap-2">
+          <Text size="sm">Foto de perfil</Text>
+          <Avatar
+            src={session.data?.user.avatar_url}
+            alt={session.data?.user.name}
+          />
+        </label>
         <label className="flex flex-col gap-2">
           <Text size="sm">Sobre você</Text>
           <TextArea prefix="ignite.com/" {...register('bio')} />
@@ -53,7 +69,7 @@ export default function TimerIntervals() {
         <span className="text-Gray-200">
           Fale um pouco sobre você. Isto será exibido em sua página pessoal.
         </span>
-        <Button>
+        <Button disabled={isSubmitting}>
           Finalizar
           <ArrowRight />
         </Button>
