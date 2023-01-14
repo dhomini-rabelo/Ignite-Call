5955246fe4fd8d7e78044aa6920ae02633fa9137
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-01-14 18:02:00 -0300

    add dynamic date with dayjs

--- stage06/src/pages/schedule/[username]/components/FormSteps/CalendarStep/index.tsx
+++ stage06/src/pages/schedule/[username]/components/FormSteps/CalendarStep/index.tsx
@@ -1,4 +1,5 @@
 import { Text } from '@ignite-ui/react'
+import dayjs from 'dayjs'
 import { useState } from 'react'
 import { Calendar } from '../../../../../../layout/components/Calendar'
 import { Div } from './styles'
@@ -7,6 +8,11 @@ export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
   const dateIsSelected = selectedDate !== null
 
+  const weekDayInfo = selectedDate && {
+    name: dayjs(selectedDate).format('dddd'),
+    dayDescription: dayjs(selectedDate).format('DD[ de ]MMMM'),
+  }
+
   return (
     <Div.container
       isTimePickerOpen={dateIsSelected}
@@ -14,9 +20,10 @@ export function CalendarStep() {
     >
       <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
       {dateIsSelected && (
-        <div className="border-l border-Gray-600 pt-6 px-6 overflow-y-scroll absolute top-0 bottom-0 right-0">
+        <div className="border-l border-Gray-600 pt-6 px-6 overflow-y-scroll absolute top-0 bottom-0 right-0 w-[280px]">
           <Text size="md">
-            terça-feira <span className="text-Gray-200">20 de setembro</span>
+            {weekDayInfo!.name}{' '}
+            <span className="text-Gray-200">{weekDayInfo!.dayDescription}</span>
           </Text>
           <Div.timePicker className="mt-3 grid gap-2">
             <button>08:00h</button>
