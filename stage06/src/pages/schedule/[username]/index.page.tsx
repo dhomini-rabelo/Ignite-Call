ecf13bff8fe75832e4f3354ba6b64748b7c7e47d
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-01-13 22:32:05 -0300

    start schedule page with GetStaticProps

---
+++ stage06/src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,57 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '../../../code/settings/backend'
+
+interface Props {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: Props) {
+  return (
+    <main className="max-w-[852px] px-4 mx-auto mt-20 mb-4">
+      <div className="flex flex-col items-center">
+        <Avatar src={user.avatarUrl} alt={user.name} />
+        <Heading className="leading-relaxed mt-2">{user.name}</Heading>
+        <Text className="text-Gray-200">{user.bio}</Text>
+      </div>
+    </main>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
