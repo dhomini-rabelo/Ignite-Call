098033ecc48a965df7cfa65b0f30d4e143e6a7c0
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-01-14 23:50:57 -0300

    start api for get blocked dates

---
+++ stage06/src/pages/api/users/[username]/blocked-dates.api.ts
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../../code/settings/backend'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+  const { month, year } = req.query
+
+  if (!month || !year) {
+    return res.status(400).json({
+      message: "Year or month don't sended",
+    })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(404).json({
+      message: 'User not found',
+    })
+  }
+
+  const userAvailability = await prisma.userTimeInterval.findMany({
+    select: {
+      week_day: true,
+    },
+    where: {
+      user_id: user.id,
+    },
+  })
+
+  const daysWithoutAvailability = [0, 1, 2, 3, 4, 5, 6].filter((weekDay) =>
+    userAvailability.some(
+      (timeAvailability) => timeAvailability.week_day === weekDay,
+    ),
+  )
+
+  return res.status(200).json({
+    blockedWeekDays: daysWithoutAvailability,
+  })
+}
