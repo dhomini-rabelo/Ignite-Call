9af83fadc02ecdc470619b43e40472c2145196d6
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-06-01 23:54:05 -0300

    config tailwind

--- practice06/src/pages/_app.tsx
+++ practice06/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import '../layout/assets/styles/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
