e47cad8d1879c80b9cd48c8ed25b8e474346ee6f
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-06-08 22:40:53 -0300

    add home page

--- practice06/src/pages/index.tsx
+++ practice06/src/pages/index.tsx
@@ -1,10 +1,47 @@
 import Image from 'next/image'
-
+import HomeImage from '@/layout/assets/images/home.png'
+import GoogleIcon from '@/layout/assets/images/google.svg'
+import RocketIcon from '@/layout/assets/images/rocket.svg'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className='text-blue-600'>Hello World!!!!</h1>
-    </div>
+    <main className="grow flex justify-center bg-Gray-800 py-5 text-white">
+      <Image
+        src={HomeImage}
+        width={598}
+        height={912}
+        alt="Logo BookWise com fundo colorido e mulher lendo enquanto relaxa"
+      />
+      <div className="flex items-center justify-center px-56 pb-14">
+        <section className="flex flex-col w-96">
+          <h2 className="text-2xl">
+            <strong>Boas vindas!</strong>
+          </h2>
+          <span className="text-Gray-200">
+            Faça seu login ou acesse como visitante.
+          </span>
+          <div className="flex flex-col w-full mt-10 gap-y-4">
+            <button className="w-full bg-Gray-600 rounded-lg flex items-center gap-x-5 text-Gray-200 py-5 px-6">
+              <Image
+                src={GoogleIcon}
+                width={32}
+                height={32}
+                alt="Logo do Google"
+              />
+              <strong>Entrar com Google</strong>
+            </button>
+            <button className="w-full bg-Gray-600 rounded-lg flex items-center gap-x-5 text-Gray-200 py-5 px-6">
+              <Image
+                src={RocketIcon}
+                width={32}
+                height={32}
+                alt="Ícone de foguete"
+              />
+              <strong>Acessar como visitante</strong>
+            </button>
+          </div>
+        </section>
+      </div>
+    </main>
   )
 }
--- practice06/tailwind.config.js
+++ practice06/tailwind.config.js
@@ -6,11 +6,45 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      md: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        white: '#FFFFFF',
+        black: '#000000',
+
+        Green: {
+          100: '#50B2C0',
+          200: '#255D6A',
+          300: '#0A313C',
+        },
+
+        Purple: {
+          100: '#8381D9',
+          200: '#2A2879',
+        },
+
+        Gray: {
+          100: '#F8F9FC',
+          200: '#E6E8F2',
+          300: '#D1D6E4',
+          400: '#8D95AF',
+          500: '#303F73',
+          600: '#252D4A',
+          700: '#181C2A',
+          800: '#0E1116',
+        },
       },
     },
   },
