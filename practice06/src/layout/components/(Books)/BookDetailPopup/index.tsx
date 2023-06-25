deb09839ffb996a0fadf16bd00a48b30a107d164
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-06-25 15:42:42 -0300

    add comment

--- practice06/src/layout/components/(Books)/BookDetailPopup/index.tsx
+++ practice06/src/layout/components/(Books)/BookDetailPopup/index.tsx
@@ -1,4 +1,6 @@
-import { BookOpen, BookmarkSimple, X } from '@phosphor-icons/react'
+import { BookOpen, BookmarkSimple, Star, X } from '@phosphor-icons/react'
+import UserIcon from '@/layout/assets/images/user.svg'
+import Image from 'next/image'
 import { SimpleBook } from '../SimpleBook'
 import { Div } from './styles'
 
@@ -38,6 +40,49 @@ export function BookDetailPopup() {
               </div>
             </section>
           </div>
+        </section>
+        <section className="text-Gray-100 mt-10">
+          <header className="flex justify-between items-center">
+            <span className="text-sm leading-6 text-Gray-200">Avaliações</span>
+            <strong className="flex text-Purple-100 items-center cursor-pointer">
+              <strong className="leading-6">Avaliar</strong>
+            </strong>
+          </header>
+          <main className="mt-4 flex flex-col gap-y-3">
+            <section className="p-6 max-w-[608px] rounded-lg bg-Gray-700">
+              <header
+                className="flex justify-between gap-x-4"
+                style={{ marginBottom: '1.25rem' }}
+              >
+                <Image
+                  src={UserIcon}
+                  width={40}
+                  height={40}
+                  alt="Foto de perfil de usuário"
+                />
+                <div className="grow">
+                  <strong className="font-normal leading-relaxed block text-Gray-100">
+                    Jaxson Dias
+                  </strong>
+                  <span className="text-Gray-400 leading-relaxed font-normal text-sm block">
+                    Hoje
+                  </span>
+                </div>
+                <div className="flex text-Purple-100 gap-x-1">
+                  <Star size={16} weight="fill" />
+                  <Star size={16} weight="fill" />
+                  <Star size={16} weight="fill" />
+                  <Star size={16} weight="fill" />
+                  <Star size={16} />
+                </div>
+              </header>
+              <p className="text-Gray-300 leading-6 font-normal text-justify block">
+                Semper et sapien proin vitae nisi. Feugiat neque integer donec
+                et aenean posuere amet ultrices. Cras fermentum id pulvinar
+                velit ipsum. Sed vulputate massa velit nibh...
+              </p>
+            </section>
+          </main>
         </section>
       </Div.popup>
     </Div.container>
