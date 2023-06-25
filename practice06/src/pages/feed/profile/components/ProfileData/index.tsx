2697968fc858f2a2248051b1871dc1b3bb189445
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-06-25 19:59:17 -0300

    add profile data

--- practice06/src/pages/feed/profile/components/ProfileData/index.tsx
+++ practice06/src/pages/feed/profile/components/ProfileData/index.tsx
@@ -1,5 +1,9 @@
 import UserIcon from '@/layout/assets/images/user.svg'
+import BooksIcon from '@/layout/assets/images/books.svg'
+import AuthorIcon from '@/layout/assets/images/author.svg'
 import Image from 'next/image'
+import { Div } from './styles'
+import { BookOpen, BookmarkSimple } from '@phosphor-icons/react'
 
 export default function ProfileData() {
   return (
@@ -18,6 +22,45 @@ export default function ProfileData() {
           membro desde 2019
         </span>
       </div>
+      <Div.line className="my-8" />
+      <main className="py-5 flex flex-col gap-y-10">
+        <section className="flex gap-x-5 items-center">
+          <BookOpen size={24} className="text-Green-100" />
+          <div className="flex flex-col">
+            <strong className="leading-5 text-Gray-200">3800</strong>
+            <label className="leading-6 text-Gray-300 text-sm">
+              Páginas lidas
+            </label>
+          </div>
+        </section>
+        <section className="flex gap-x-5 items-center">
+          <Image src={BooksIcon} width={24} height={24} alt="ícone de livros" />
+          <div className="flex flex-col">
+            <strong className="leading-5 text-Gray-200">10</strong>
+            <label className="leading-6 text-Gray-300 text-sm">
+              Livros avaliados
+            </label>
+          </div>
+        </section>
+        <section className="flex gap-x-5 items-center">
+          <Image src={AuthorIcon} width={24} height={24} alt="ícone de autor" />
+          <div className="flex flex-col">
+            <strong className="leading-5 text-Gray-200">8</strong>
+            <label className="leading-6 text-Gray-300 text-sm">
+              Autores lidos
+            </label>
+          </div>
+        </section>
+        <section className="flex gap-x-5 items-center">
+          <BookmarkSimple size={24} className="text-Green-100" />
+          <div className="flex flex-col">
+            <strong className="leading-5 text-Gray-200">Computação</strong>
+            <label className="leading-6 text-Gray-300 text-sm">
+              Categoria mais lida
+            </label>
+          </div>
+        </section>
+      </main>
     </aside>
   )
 }
---
+++ practice06/src/pages/feed/profile/components/ProfileData/styles.ts
@@ -0,0 +1,11 @@
+import { styled } from '@stitches/react'
+
+export const Div = {
+  line: styled('div', {
+    width: '2rem',
+    height: '0.25rem',
+    borderRadius: '999px',
+    background:
+      'var(--gradient-horizontal, linear-gradient(90deg, #7FD1CC 0%, #9694F5 100%))',
+  }),
+}
