879f01c2fc3460dac09ede9a1b87dd90e10b60f6
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-07-02 13:03:34 -0300

    FEAT: use suspense API

--- practice06/src/app/feed/books/page.tsx
+++ practice06/src/app/feed/books/page.tsx
@@ -2,7 +2,7 @@
 import { BookDetailPopup } from '@/layout/components/(Books)/BookDetailPopup'
 import { Books } from './components/Books'
 import { Search } from './components/Search'
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 
 export default function BooksPage() {
   const [bookPopupIsOpen, setBookPopupIsOpen] = useState(true)
@@ -15,7 +15,9 @@ export default function BooksPage() {
     <>
       <main className="pt-14 grow max-w-[996px] ml-24">
         <Search />
-        <Books />
+        <Suspense>
+          <Books />
+        </Suspense>
       </main>
       {bookPopupIsOpen && <BookDetailPopup handleClose={closePopup} />}
     </>
