6522bb49deee556a884c97850dff9c336d44251a
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-07-02 20:58:32 -0300

    FEAT: add BooksWrapper because is Server component

--- practice06/src/app/feed/books/page.tsx
+++ practice06/src/app/feed/books/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 import { BookDetailPopup } from '@/layout/components/(Books)/BookDetailPopup'
-import { Books } from './components/Books'
 import { Search } from './components/Search'
 import { Suspense, useState } from 'react'
+import { BooksWrapper } from './components/Books/wrapper'
 
 export default function BooksPage() {
   const [bookPopupIsOpen, setBookPopupIsOpen] = useState(false)
@@ -16,7 +16,7 @@ export default function BooksPage() {
       <main className="pt-14 grow max-w-[996px] ml-24">
         <Search />
         <Suspense>
-          <Books />
+          <BooksWrapper />
         </Suspense>
       </main>
       {bookPopupIsOpen && <BookDetailPopup handleClose={closePopup} />}
