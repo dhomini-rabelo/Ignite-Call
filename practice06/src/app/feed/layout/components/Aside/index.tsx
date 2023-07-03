3b270a853fcb0cbeecc7dc07f5129f9ba15ac608
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-07-03 20:09:48 -0300

    FIX: max height in aside

--- practice06/src/app/feed/layout/components/Aside/index.tsx
+++ practice06/src/app/feed/layout/components/Aside/index.tsx
@@ -10,48 +10,50 @@ export default function AsideNav() {
   const path = usePathname()
 
   return (
-    <aside className="ml-5 bg-Gray-700 pt-10 my-5 pb-6 flex flex-col items-center justify-between px-14">
-      <div>
-        <Image
-          src={LogoIcon}
-          width={128}
-          height={32}
-          alt="Logo do projeto BookWise"
-        />
-        <nav className="mt-16 flex flex-col gap-y-4">
-          <A.NavLink
-            active={path === '/feed'}
-            href="/feed"
-            className="flex gap-x-3 ml-4 relative"
-          >
-            <div className="bar" />
-            <ChartLineUp size={24} />
-            <strong className="leading-relaxed">Início</strong>
-          </A.NavLink>
-          <A.NavLink
-            active={path === '/feed/books'}
-            href="/feed/books"
-            className="flex gap-x-3 ml-4 relative"
-          >
-            <div className="bar" />
-            <Binoculars size={24} />
-            <strong className="leading-relaxed">Explorar</strong>
-          </A.NavLink>
-          <A.NavLink
-            active={path === '/feed/profile'}
-            href="/feed/profile"
-            className="flex gap-x-3 ml-4 relative"
-          >
-            <div className="bar" />
-            <User size={24} />
-            <strong className="leading-relaxed">Perfil</strong>
-          </A.NavLink>
-        </nav>
+    <aside className="max-h-[95vh] my-5">
+      <div className="bg-Gray-700 ml-5 pt-10 pb-6 flex flex-col items-center justify-between px-14 h-full">
+        <div>
+          <Image
+            src={LogoIcon}
+            width={128}
+            height={32}
+            alt="Logo do projeto BookWise"
+          />
+          <nav className="mt-16 flex flex-col gap-y-4">
+            <A.NavLink
+              active={path === '/feed'}
+              href="/feed"
+              className="flex gap-x-3 ml-4 relative"
+            >
+              <div className="bar" />
+              <ChartLineUp size={24} />
+              <strong className="leading-relaxed">Início</strong>
+            </A.NavLink>
+            <A.NavLink
+              active={path === '/feed/books'}
+              href="/feed/books"
+              className="flex gap-x-3 ml-4 relative"
+            >
+              <div className="bar" />
+              <Binoculars size={24} />
+              <strong className="leading-relaxed">Explorar</strong>
+            </A.NavLink>
+            <A.NavLink
+              active={path === '/feed/profile'}
+              href="/feed/profile"
+              className="flex gap-x-3 ml-4 relative"
+            >
+              <div className="bar" />
+              <User size={24} />
+              <strong className="leading-relaxed">Perfil</strong>
+            </A.NavLink>
+          </nav>
+        </div>
+        <Link href="/" className="flex gap-x-3 items-center">
+          <strong className="text-Gray-100">Fazer Login</strong>
+          <SignIn size={20} className="text-Green-100" />
+        </Link>
       </div>
-      <Link href="/" className="flex gap-x-3 items-center">
-        <strong className="text-Gray-100">Fazer Login</strong>
-        <SignIn size={20} className="text-Green-100" />
-      </Link>
     </aside>
   )
 }
