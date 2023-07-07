ca8841be39a6cdf73073288e8aecf311fd45df44
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-07-07 20:25:25 -0300

    FEAT: show dynamic data in SimpleRating

--- practice06/src/layout/components/(Rating)/SimpleRating/index.tsx
+++ practice06/src/layout/components/(Rating)/SimpleRating/index.tsx
@@ -1,11 +1,31 @@
 import { Star } from '@phosphor-icons/react'
-import UserIcon from '@/layout/assets/images/user.svg'
 import Image from 'next/image'
+import { IUserModel } from '@/code/db/users'
 
 export function SimpleRating({
   currentUser = false,
+  rating = {
+    created_at: '2023-07-04T00:01:33.807Z',
+    description: 'Nec tempor nunc in egestas.',
+    id: 'aecfc87c-ca03-47af-b524-4ea93dbdc889',
+    rate: 4,
+    user: {
+      avatar_url:
+        'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80',
+      created_at: '2023-07-04T00:01:33.807Z',
+      id: '48e458c0-8b1e-4994-b85a-1e1cfcc9dd60',
+      name: 'Jaxson Dias',
+    },
+  },
 }: {
   currentUser?: boolean
+  rating?: {
+    id: string
+    user: IUserModel
+    rate: number
+    description: string
+    created_at: string
+  }
 }) {
   return (
     <section
@@ -17,14 +37,14 @@ export function SimpleRating({
         style={{ marginBottom: '1.25rem' }}
       >
         <Image
-          src={UserIcon}
+          src={rating.user.avatar_url}
           width={40}
           height={40}
           alt="Foto de perfil de usuário"
         />
         <div className="grow">
           <strong className="leading-relaxed block text-Gray-100">
-            Jaxson Dias
+            {rating.user.name}
           </strong>
           <span className="text-Gray-400 leading-relaxed font-normal text-sm block">
             Hoje
@@ -39,9 +59,7 @@ export function SimpleRating({
         </div>
       </header>
       <p className="text-Gray-300 leading-6 font-normal text-justify block">
-        Semper et sapien proin vitae nisi. Feugiat neque integer donec et aenean
-        posuere amet ultrices. Cras fermentum id pulvinar velit ipsum. Sed
-        vulputate massa velit nibh...
+        {rating.description}
       </p>
     </section>
   )
