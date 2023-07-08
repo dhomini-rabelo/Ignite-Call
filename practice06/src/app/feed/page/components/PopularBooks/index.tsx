ef719d4f0cccac770c5597c0a0be305e544b59ef
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-07-08 16:06:14 -0300

    list popular books

--- practice06/src/app/feed/page/components/PopularBooks/index.tsx
+++ practice06/src/app/feed/page/components/PopularBooks/index.tsx
@@ -1,10 +1,11 @@
 'use client'
 
+import { IBooksData } from '@/layout/client/types'
 import { SimpleBook } from '@/layout/components/(Books)/SimpleBook'
 import { CaretRight } from '@phosphor-icons/react'
 import Link from 'next/link'
 
-export default function PopularBooks() {
+export default function PopularBooks({ booksData }: { booksData: IBooksData }) {
   return (
     <aside className="pt-32 grow max-w-[324px]">
       <header className="flex justify-between items-center">
@@ -16,8 +17,12 @@ export default function PopularBooks() {
           <CaretRight size={16} />
         </strong>
       </header>
-      <main className="mt-4">
-        <SimpleBook width={64} height={94} />
+      <main className="mt-4 flex flex-col gap-y-3">
+        {booksData.books
+          .slice(0, booksData.books.length > 4 ? 4 : booksData.books.length)
+          .map((book) => (
+            <SimpleBook width={64} height={94} book={book} key={book.id} />
+          ))}
       </main>
     </aside>
   )
