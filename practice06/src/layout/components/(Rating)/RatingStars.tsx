78526a764e4fd84818c34cd9ce8b06201fb1fb84
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-07-08 00:27:04 -0300

    generate uuid v4 for key attr

--- practice06/src/layout/components/(Rating)/RatingStars.tsx
+++ practice06/src/layout/components/(Rating)/RatingStars.tsx
@@ -1,11 +1,12 @@
 import { Star } from '@phosphor-icons/react'
+import { v4 as uuidV4 } from 'uuid'
 
 export function RatingStars({ rate }: { rate: number }) {
   return (
     <div className="flex text-Purple-100 gap-x-1">
       {[1, 2, 3, 4, 5].map((value) =>
         value <= rate ? (
-          <Star size={16} weight="fill" key={value} />
+          <Star size={16} weight="fill" key={uuidV4()} />
         ) : (
           <Star size={16} key={value} />
         ),
