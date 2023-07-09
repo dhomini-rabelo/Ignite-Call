79b83a0753530885baad0fce32c2427166d934e4
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-07-09 17:55:34 -0300

    generate email too

--- practice06/prisma/seed.mjs
+++ practice06/prisma/seed.mjs
@@ -18,7 +18,8 @@ async function main() {
       data: {
         id: user.id,
         name: user.name,
-        avatar_url: user.avatar_url,
+        image: user.avatar_url,
+        email: `${user.name}@dev.com`,
       },
     })
   })
