2341f9918a4e782af4100377f56f9e201e39dddb
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-07-09 21:18:06 -0300

    use SessionProvider

--- practice06/src/app/feed/books/page.tsx
+++ practice06/src/app/feed/books/page.tsx
@@ -3,10 +3,11 @@ import { BookDetailPopup } from '@/layout/components/(Books)/BookDetailPopup'
 import { Search } from './components/Search'
 import { Suspense } from 'react'
 import { BooksWrapper } from './components/Books/wrapper'
+import { SessionProvider } from 'next-auth/react'
 
 export default function BooksPage() {
   return (
-    <>
+    <SessionProvider>
       <main className="pt-14 grow max-w-[996px] ml-24 popup-overflow">
         <Search />
         <Suspense>
@@ -14,6 +15,6 @@ export default function BooksPage() {
         </Suspense>
       </main>
       <BookDetailPopup />
-    </>
+    </SessionProvider>
   )
 }
--- practice06/src/app/feed/page/clientWrapper.tsx
+++ practice06/src/app/feed/page/clientWrapper.tsx
@@ -1,18 +1,19 @@
 'use client'
 
+import { SessionProvider } from 'next-auth/react'
 import { IBooksData } from '../../../layout/client/types'
 import Feed from './components/Feed'
 import PopularBooks from './components/PopularBooks'
 import { BookDetailPopup } from '@/layout/components/(Books)/BookDetailPopup'
 
-export async function FeedPageClientWrapper({ data }: { data: IBooksData }) {
+export function FeedPageClientWrapper({ data }: { data: IBooksData }) {
   return (
-    <>
+    <SessionProvider>
       <main className="flex grow popup-overflow">
         <Feed booksData={data} />
         <PopularBooks booksData={data} />
       </main>
       <BookDetailPopup />
-    </>
+    </SessionProvider>
   )
 }
