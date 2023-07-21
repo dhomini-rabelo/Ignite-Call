0dc3c045396b9f1e583f4bd0e4667b71a3e205ce
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-07-20 22:59:06 -0300

    add GET route for get user ratings

---
+++ practice06/src/app/api/ratings/user/route.ts
@@ -0,0 +1,29 @@
+import { prisma } from '@/code/settings/backend'
+import { NextResponse as res } from 'next/server'
+
+export async function GET(request: Request) {
+  const { email } = await request.json()
+
+  if (!email) {
+    return res.json(
+      { message: 'Email do usuário não informado' },
+      { status: 400 },
+    )
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  })
+
+  if (!user) {
+    return res.json({ message: 'Usuário não encontrado' }, { status: 400 })
+  }
+
+  const ratings = await prisma.rating.findMany({
+    where: {
+      user,
+    },
+  })
+
+  return res.json({ data: ratings })
+}
