8827c3fd6321007ff4bc293ab8c4d8e25dbab331
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-07-22 20:33:59 -0300

    dynamic Profile page

--- practice06/src/app/feed/profile/page.tsx
+++ practice06/src/app/feed/profile/page.tsx
@@ -3,8 +3,23 @@ import { ProfilePageClientWrapper } from './clientWrapper'
 import { getServerSession } from 'next-auth'
 import { getUserRatingsData } from '@/layout/client/ratings'
 
-export default async function ProfilePage() {
-  const session = await getServerSession(authOptions)
-  const data = await getUserRatingsData(session?.user?.email || '')
+interface Props {
+  searchParams: {
+    user?: string
+  }
+}
+
+export default async function ProfilePage(props: Props) {
+  async function getData(email: string | undefined) {
+    if (email) {
+      return await getUserRatingsData(email)
+    } else {
+      const session = await getServerSession(authOptions)
+      return await getUserRatingsData(session?.user?.email || '')
+    }
+  }
+
+  const data = await getData(props.searchParams.user)
+
   return <ProfilePageClientWrapper data={data} />
 }
